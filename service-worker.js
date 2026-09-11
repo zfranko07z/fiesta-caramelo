97b57361d8311f6043175039d6405706b20491b2
zfranko07z <38497164+zfranko07z@users.noreply.github.com>
2026-09-11 16:08:40 -0400

    fiesta-caramelo V1
    Prototipo de 20 niveles

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'fiesta-caramelo-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request);
+    })
+  );
+});
